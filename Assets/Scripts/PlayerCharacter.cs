9e1b95d7227043aea7dc988002a4bcb5cae9a34a
Ян <lsvjhm@gmail.com>
2023-07-31 16:42:14 +0300

    W2 lesson1

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour
 {
+    [SerializeField] private Rigidbody _rigidbody;
     [SerializeField] private float _speed = 2f;
     private float _inputH;
     private float _inputV;
-    void Update()
+    void FixedUpdate()
     {
         Move();
     }
@@ -19,8 +20,10 @@ public class PlayerCharacter : MonoBehaviour
     }
     private void Move()
     {
-        Vector3 direction = new Vector3(_inputH, 0, _inputV).normalized;
-        transform.position += direction * Time.deltaTime * _speed;
+        //Vector3 direction = new Vector3(_inputH, 0, _inputV).normalized;
+        //transform.position += direction * Time.deltaTime * _speed;
+        Vector3 velocity = (transform.forward * _inputV + transform.right * _inputH).normalized * _speed;
+        _rigidbody.velocity = velocity;
     }
     public void GetMoveInfo(out Vector3 position)
     {
